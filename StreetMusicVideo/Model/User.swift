44da41f3ed9410abe433783aca166e2b077c9731
Henrique Velloso <henriquevelloso@gmail.com>
2015-09-01 15:33:01 -0300

    first commit

---
+++ StreetMusicVideo/Model/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  StreetMusicVideo
+//
+//  Created by Henrique Velloso on 9/1/15.
+//  Copyright © 2015 HenriqueVelloso. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+
+}
